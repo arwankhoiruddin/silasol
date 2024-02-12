afb20ecabc8fdceb6fc1940f87cc39023517d3a5
jun-fajr <jun.fajr@gmail.com>
2024-02-12 17:56:03 +0700

    add admin page

--- silasol/app/page.tsx
+++ silasol/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Dashboard from "@/components/Dashboard/Dashboard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Silasol",
+  description: "Sistem Informasi Pelayanan Sosial MPKS PWM DIY",
+  // other metadata
+};
 
 export default function Home() {
   return (
     <>
-    <p>Hello</p>
+      <Dashboard />
     </>
   );
 }
